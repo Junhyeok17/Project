6afe06b5f934b6e955c816e3fed3553692802cac
주재완 <yjjw2000@naver.com>
2022-08-13 22:11:39 +0900

    first commit

--- app/src/main/java/joo/jae/wan/main/MainActivity.java
+++ app/src/main/java/joo/jae/wan/main/MainActivity.java
@@ -3,6 +3,7 @@ package joo.jae.wan.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import joo.jae.wan.R;
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Jaewan", "Log");
     }
 }
