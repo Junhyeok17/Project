c3bc949245e8071e452ff30be1b8391620bdcc20
Junhyun_Kang <kangjunhyun38@gmail.com>
2022-08-25 22:17:56 +0900

    Search_xml_recycler

---
+++ app/src/main/java/joo/jae/wan/main/Search.java
@@ -0,0 +1,28 @@
+package joo.jae.wan.main;
+
+public class Search {
+    String name;
+    String address;
+
+    public Search(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
